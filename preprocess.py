7b8e1942b77c0656288b6a84fe1b705f1e6265b8
pupadhya <prajna-devi.upadhyay@inria.fr>
2023-08-03 21:46:22 +0530

    saving

--- preprocess.py
+++ preprocess.py
@@ -7,8 +7,9 @@ o = open(sys.argv[2], "w")
 for line in f:
     line = line[:-1]
     if line.startswith("NONE") or line.startswith("CP_START") or line.startswith("CP") or line.startswith(
-            "CC") or line.startswith("OTHERS"):
+            "CC") or line.startswith("OTHERS") or line == "":
         continue
     else:
-        o.write(line+"\n")
+        o.write("#" + line + "\n")
+        o.write("JARGON\n")
 o.close()
