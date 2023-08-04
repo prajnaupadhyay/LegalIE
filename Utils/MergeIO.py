d8832251a50c6440b0d2df08acd5eb0de34118e4
chaitra1318 <44636735+chaitra1318@users.noreply.github.com>
2023-08-04 11:07:54 +0530

    Add files via upload
    This is used for merging input and output files while creating coordination sentences at every level

---
+++ Utils/MergeIO.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Removes spl character and line starting with Testing
+fileX = "TestNotFormatted.txt"
+output1 = open("TestFlattenedTreeWithoutSplChar1.txt","w")
+with open(fileX, "r") as file:
+    for line in file:
+        if line.startswith(" Testing"):
+            if "COORDINATION(" in line:
+                index = line.find("COORDINATION( ")
+                #print(index)
+            elif "NONE" in line:
+                index = line.find("NONE")
+            output1.write(line[index:])
+        elif not line.startswith("Testing "):
+            output1.write(line)
+            
+            #print(line[index:])
+
+
+# In[6]:
+
+
+#Merge two files
+#file1 = "sampleinput.txt"
+file1 = "testSent.txt"
+#file2 = "sampleflatcoordsTree.txt"
+#file2= "/Users/chaitrakaustubh/CARB_SEntences/openIETRainConj/slurm.44402.out_allFlattened.txt"
+file2 = "TestFlattenedTreeWithoutSplChar1.txt"
+file3=open("TestmergeIO.txt", "w+")
+with open(file1,"r") as f1, open(file2,"r") as f2:
+    for line, coords in zip(f1,f2):
+        #print("#"+line)
+        file3.write("\n"+'#'+line)
+        
+        #for coords in f2:
+        #print(coords)
+        file3.write(coords+"\n")
+            
+        
+
+
+# In[7]:
+
+
+#Formatted MergedFile - Adds brackets
+infile = "TestmergeIO.txt"
+output = open("test.txt","w")
+    
+with open(infile,"r") as file:
+    for line in file:
+        if line.startswith('#') and len(line) > 3:
+            #print(len(line),line)
+            output.write(line)
+        elif line.startswith("COORDINATION("):
+            brackets = line.count("COORDINATION(", 0, len(line))
+            line = line.replace("NONE", ")"*brackets)
+            #print(line)
+            output.write(line)
+        
+
+
+# In[9]:
+
+
+count=0
+with open("test.txt","r") as file:
+    for line in file:
+        if line.startswith('#'):
+            count = count+1
+print(count)
+
+
+# In[1]:
+
+
+#Adds NONE between two consecutive lines starting with #
+def add_none_between_lines(file_path):
+    lines = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    modified_lines = []
+    for i in range(len(lines)):
+        line = lines[i]
+        modified_lines.append(line.strip())
+        if i < len(lines) - 1 and line.startswith('#') and lines[i + 1].startswith('#'):
+            modified_lines.append('NONE')
+
+    with open(file_path, 'w') as file:
+        file.write('\n'.join(modified_lines))
+
+if __name__ == "__main__":
+    file_path = "test.txt"
+    add_none_between_lines(file_path)
+
+
+# In[ ]:
+
+
+
+
