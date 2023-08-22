2f675f09cc347c36445763ca551b10e38d8759c1
chaitra1318 <44636735+chaitra1318@users.noreply.github.com>
2023-08-22 09:02:16 +0530

    Update computeRogue.py

--- Utils/computeRogue.py
+++ Utils/computeRogue.py
@@ -6,41 +6,52 @@ import numpy as np
 def calculate_rouge_score(reference_file, prediction_file):
     test_labels = []
     predictions = []
-    #with open(reference_file, 'r') as ref_file:
-     #   references = ref_file.readlines()
+    #REfernce file 
     with open(reference_file, 'r') as file:
         for line in file:
             if not line.startswith('#'):
                 if "COORDINATION(" in line:
                     line = line.replace("COORDINATION(", "")
-                    
-                    #print(line[:-3].strip()
-                    line = line[:-3].strip()
+                    line = line.replace(')', '')
                     test_labels.append(line)
                 else:
                     test_labels.append(line)
-                          
-                
-    test_labels = np.array(test_labels)
+                    
+    test_labesl = np.array(test_labels)
+   
             
 
         
 
-    #with open(prediction_file, 'r') as pred_file:
-     #   predictions = pred_file.readlines()
-    with open(prediction_file, 'r') as file:
-        for line in file:
-            if line.startswith('Prediction'):
-                line = line.strip("Prediction:")
-                if "COORDINATION(" in line:
-                    line = line.replace("COORDINATION(", "")
-                    line = line[:-3]
-                    predictions.append(line)
+    #Predictions File - BART
+#     with open(prediction_file, 'r') as file:
+#         for line in file:
+#             if line.startswith('Prediction'):
+#                 line = line.strip("Prediction:")
+#                 if "COORDINATION(" in line:
+#                     line = line.replace("COORDINATION(", "")
+#                     line = line.replace("COORDINATIONAL(", '')
+#                     line = line.replace(')','')
+#                     #line = line.replace(',None','')
+                
+#                     predictions.append(line)
                    
-                else:
-                    predictions.append(line)
-    predictions = np.array(predictions)
+#                 else:
+#                     predictions.append(line)
 
+                    
+    
+                
+#     predictions = np.array(predictions)
+    #Processing Conj file
+    with open(prediction_file, "r") as file:
+        for line in file:
+            if not line.startswith('#') and line != '\n':
+                predictions.append(line.strip()[:-1])
+                
+ 
+    
+    print("Total number of test and predictions:",len(test_labels), len(predictions))
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL']) #, use_stemmer=True
 
     rouge_scores = {
@@ -66,16 +77,21 @@ def calculate_rouge_score(reference_file, prediction_file):
 
 
 
-reference_file_path = 'ptb_test.txt'
-prediction_file_path = 'prediction_ptb_Level.txt'
+reference_file_path = '/Users/chaitrakaustubh/LegalIE/LegalIE-master/data/NewDataset/coordination_tree_encoding.txt'
+prediction_file_path = '/Users/chaitrakaustubh/LegalIE/LegalIE-master/data/NewDataset/ConvertedConj.txt'
 
 test_labels = []
 predictions = []
 
 rouge_scores = calculate_rouge_score(reference_file_path, prediction_file_path)
-
+result = open("Result_Conj.txt", "w")
 for metric, values in rouge_scores.items():
     print(f"{metric}:")
     print(f"  Precision: {values['p']:.4f}")
     print(f"  Recall: {values['r']:.4f}")
     print(f"  F1 Score: {values['f']:.4f}")
+    
+    result.write(f"{metric}:"+"\n\n")
+    result.write(f"  Precision: {values['p']:.4f}"+"\n")
+    result.write(f"  Recall: {values['r']:.4f}"+"\n")
+    result.write(f"  F1 Score: {values['f']:.4f}"+"\n")
