28268633cb14b8121757942d48c9a0438ac0637b
Sankalp <sankalpkulkarni02@gmail.com>
2023-10-06 19:27:15 +0530

    Level f1 correcetd

--- eval.bash
+++ eval.bash
@@ -2,5 +2,5 @@
 
 for n in 03 16 24 32
 do
-    python3 Utils/level_scores.py T5 data/CoordinationDataSet/gold/test_copy.coord data/CoordinationDataSet/output2/predictions/Prediction_T5_base_b${n}.coord > data/CoordinationDataSet/output2/evaluations/wire57_f1_level/Result_T5_base_b${n}_wire57v3.txt
+    python3 Utils/level_scores.py BART data/CoordinationDataSet/gold/test_copy.coord data/CoordinationDataSet/output2/predictions/Prediction_BART_base_b${n}_l1.coord > data/CoordinationDataSet/output2/evaluations/wire57_f1_level/Result_BART_base_b${n}_l1_wire57v3.txt
 done
